7fa73cfdc8afe06ea88850683e10675cdd724bc2
yitzhak <matias6@gmail.com>
2024-08-12 14:28:36 -0400

    ecommerce

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,5 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import {BrowserRouter as Router, Routes, Route, Navigate} from 'react-router-dom';
 import { Box } from '@chakra-ui/react';
 import ProductList from './components/ProductList';
 import ProductDetail from './components/ProductDetail';
@@ -9,6 +9,7 @@ function App() {
         <Box p={4}>
             <Router>
                 <Routes>
+                    <Route path="/" element={<Navigate replace to="/products" />} />
                     <Route path="/products" element={<ProductList />} />
                     <Route path="/product/:id/:brand" element={<ProductDetail />} />
                 </Routes>
